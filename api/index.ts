1b838a3c16c886de7f7a15ea133854e5f6efc5a3
Kaiden Riley <kaiden01@outlook.de>
2025-05-11 12:52:49 +0100

    init

--- api/index.ts
+++ api/index.ts
@@ -7,11 +7,16 @@ export const config = {
 
 const app = new Hono().basePath('/api')
 
-app.get('/', async (c) => {
-  const rawData = await fetch('https://api.henrikdev.xyz/valorant/v2/mmr/eu/DreaLeFay/9031?api_key=HDEV-5cdfb84f-9133-4ce3-a27b-db30845e6f17')
+app.get('/rank/:name/:tag', async (c) => {
+  const username = c.req.param('name')
+  const tag = c.req.param('tag')
+
+  const rawData = await fetch(`https://api.henrikdev.xyz/valorant/v2/mmr/eu/${username}/${tag}?api_key=HDEV-5cdfb84f-9133-4ce3-a27b-db30845e6f17`)
   const data = await rawData.json()
 
-  return c.json(data ? data : {data: 'Data not loaded'})
+  const rank = data.current_data.currenttierpatched
+
+  return c.json({message: `Your rank is ${rank}`})
 })
 
 export default handle(app)
