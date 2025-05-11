4e2539e0667647092fb47d8849c3b5971dd0af97
Kaiden Riley <kaiden01@outlook.de>
2025-05-11 13:07:59 +0100

    init

--- api/index.ts
+++ api/index.ts
@@ -14,8 +14,6 @@ app.get('/rank', async (c) => {
     const rawData = await fetch(`https://api.henrikdev.xyz/valorant/v2/mmr/eu/${name}/${tag}?api_key=HDEV-5cdfb84f-9133-4ce3-a27b-db30845e6f17`)
     const data = await rawData.json()
 
-    const rank = data.current_data.currenttierpatched
-
     return c.json({message: `Your rank is ${'var not init'}`, data})
   } catch (error) {
     const {message} = error as Error
