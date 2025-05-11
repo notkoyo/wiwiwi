ddb86ac606dcee6b22ad2a6c2580d5f3d14e6168
Kaiden Riley <kaiden01@outlook.de>
2025-05-11 12:40:41 +0100

    init

--- api/index.ts
+++ api/index.ts
@@ -8,7 +8,9 @@ export const config = {
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
+  const rawData = fetch('https://api.henrikdev.xyz/valorant/v2/mmr/eu/DreaLeFay/9031?api_key=HDEV-5cdfb84f-9133-4ce3-a27b-db30845e6f17')
+
+  return c.json(rawData)
 })
 
 export default handle(app)
